15a7fbb67953ce7e5da5e584ed89202ce106335b
badabab <ky040723@gmail.com>
2024-05-08 15:22:51 +0900

    json Test

--- Assets/02.Scripts/Test/JsonTest.cs
+++ Assets/02.Scripts/Test/JsonTest.cs
@@ -11,8 +11,12 @@ public class JsonTest : MonoBehaviour
         // 1. 유추해서 데이터를 담을 수 있는 클래스를 "Person" 만들어라
         // 2. 리소스 폴더로부터 person.json 내용을 읽어와라
         TextAsset textAsset = Resources.Load("person") as TextAsset;
+        string jsonText = textAsset.text;
+        Debug.Log(jsonText);
+
         // 3. 클래스 Person의 객체를 만들고 읽어온 내용을 역직렬화 해라
-        Person person = JsonUtility.FromJson<Person>(textAsset.text);
+        Person person = JsonUtility.FromJson<Person>(jsonText);
+        
         // 4. 클래스 Person의 이름, 나이, 취미들을 Debug.Log로 출력
         Debug.Log(person.Name);
         Debug.Log(person.Age);
@@ -21,12 +25,4 @@ public class JsonTest : MonoBehaviour
             Debug.Log(person.Hobby[i]);
         }
     }
-}
-
-[Serializable]
-public class Person
-{
-    public string Name;
-    public int Age;
-    public string[] Hobby;
 }
